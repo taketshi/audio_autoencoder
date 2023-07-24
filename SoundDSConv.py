a1cd12bc27cfab564b4cb912dc109b06fafea360
AthomsG <47532248+AthomsG@users.noreply.github.com>
2023-07-25 00:15:58 +0100

    Changed class as dataset is now proocessed

--- SoundDSConv.py
+++ SoundDSConv.py
@@ -8,9 +8,9 @@ from torch.utils.data import Dataset
 # Sound Dataset
 # ----------------------------
 class SoundDSConv(Dataset):
-    def __init__(self, df, sr = 8000, duration = 10):
+    def __init__(self, df, sr = 8000, duration = 8):
         self.df = df
-        self.duration = duration*1000 # 10 seconds
+        self.duration = duration*1000 # 8 seconds
         self.sr = sr
         self.channel = 1
         self.spec_size = None
@@ -40,31 +40,7 @@ class SoundDSConv(Dataset):
         aud = torchaudio.load(self.df.loc[idx]['path_to_data'])
         waveform, SAMPLE_RATE = aud
 
-        max_ms = self.duration
-        max_len = SAMPLE_RATE//1000 * max_ms
-        num_rows, sig_len = waveform.shape
-
-        # padding or truncating 
-        if (sig_len > max_len):
-          # Truncate the signal to the given length
-          waveform = waveform[:,:max_len]
-
-        elif (sig_len < max_len):
-            # Length of padding to add at the beginning and end of the signal
-            pad_begin_len = random.randint(0, max_len - sig_len)
-            pad_end_len = max_len - sig_len - pad_begin_len
-
-            # Pad with 0s
-            pad_begin = torch.zeros((num_rows, pad_begin_len))
-            pad_end = torch.zeros((num_rows, pad_end_len))
-
-            waveform = torch.cat((pad_begin, waveform, pad_end), 1)
-
         waveform = waveform[0] # mix down
-
-        # Reduce the Sample Rate
-        resampler = transforms.Resample(orig_freq=SAMPLE_RATE, new_freq=self.sr)
-        waveform = resampler(waveform)
 
         spectrogram = transforms.Spectrogram(n_fft=1024,
                                              hop_length=512)
