6c32f0fc8c7962d47ffbc4d764df2a011e1f8ab9
Luis Dias <luis.dias412@gmail.com>
2023-07-24 18:27:50 +0100

    first version of final stuff

---
+++ AutoEncoderConv.py
@@ -0,0 +1,128 @@
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import time
+
+
+class AutoEncoderConv(nn.Module):
+
+
+    def __init__(self, encoder_layers, decoder_layers):
+        super(AutoEncoderConv, self).__init__()
+
+        # encoder_layers is dictionary of the form
+        # {channels: [1, 40, 20, 10],
+        #  kernel_conv: [3, 3, 1, 2],
+        #  padding: [1, 1, 1, 3],
+        #  stride_conv: [1,1,1,1],
+        #  kernel_pool: [2, 2, 2],
+        #  stride_pool: [2, 3, 5, 2]}
+
+
+        # Create Encoder
+        encoder_layers_torch = []
+        enc_len = len(encoder_layers['channels'])
+        
+        # Loop through encoder_layers and add layers
+        for i in range(enc_len - 1):
+            encoder_layers_torch.append(nn.Conv2d(in_channels = encoder_layers['channels'][i], out_channels = encoder_layers['channels'][i+1],
+                                        kernel_size = encoder_layers['kernel_conv'][i], padding = encoder_layers['padding'][i]))
+            encoder_layers_torch.append(nn.ReLU())
+            encoder_layers_torch.append(nn.MaxPool2d(kernel_size=encoder_layers['kernel_pool'][i], stride=encoder_layers['stride_pool'][i]))
+
+        self.encoder = nn.Sequential(*encoder_layers_torch)
+
+        # decoder_layers is dictionary of the form
+        # {channels: [1, 40, 20, 10],
+        #  kernel: [3, 3, 1, 2],
+        #  padding: [1, 1, 1, 3],
+        #  stride: [1, 1, 1, 3]}
+
+        # Create Decoder
+        decoder_layers_torch = []
+        dec_len = len(decoder_layers['channels'])
+
+        # Loop through decoder_layers and add layers
+        for i in range(dec_len - 1):
+            decoder_layers_torch.append(nn.ConvTranspose2d(in_channels = decoder_layers['channels'][i], out_channels = decoder_layers['channels'][i+1],
+                                        kernel_size = decoder_layers['kernel'][i], padding = decoder_layers['padding'][i],
+                                        stride =decoder_layers['stride'][i] ))
+            # Last one does not have activation function
+            if i != dec_len - 2:
+                decoder_layers_torch.append(nn.ReLU())
+
+        self.decoder = nn.Sequential(*decoder_layers_torch)
+        
+    def forward(self, input):
+        output = self.encoder(input)
+        output = self.decoder(output)
+        return output
+
+    def train(self, dataset, criterion = 'mse', optimizer = 'sgd', lr = 0.01, batch_size = 1, epochs = 5, verbose = 5):
+        
+        data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+        # Criterion
+        if criterion == 'mse':
+            criterion = nn.MSELoss()
+        elif criterion == 'cross_entropy':
+            criterion = nn.CrossEntropyLoss()
+
+        # Optimizer
+        if optimizer == 'sgd':
+            optimizer = optim.SGD(self.parameters(), lr = lr)
+        elif optimizer == 'adam':
+            optimizer = optim.Adam(self.parameters(), lr = lr)
+
+        losses = []
+        start_time = time.time()
+        for i in range(epochs):
+            batch_n = 0
+            for data in data_loader:
+
+                # Reset Gradients
+                optimizer.zero_grad()
+
+                # Forward Pass
+                pred =  self.forward(data)
+
+
+                # Loss Function
+                loss = criterion(pred, data)
+                loss.backward()
+
+                # Upgrade Weights and Biases
+                optimizer.step()
+
+                batch_n += 1
+                if verbose != 0 and batch_n % verbose == 0:
+                    print('Epoch:', i + 1, '| Batch number:', batch_n , '| Loss:', loss.item(), '| Time:',   round(time.time() - start_time, 2), 's')
+                    start_time = time.time()
+
+
+            losses.append(loss.item())
+
+        return losses
+
+    def input_gen(self, output, rand_input, epochs = 100, optimizer = 'sgd', criterion = 'mse', lr = 0.01):
+
+        if optimizer == 'sgd':
+            optimizer = optim.SGD([rand_input], lr = lr)
+        
+        if criterion == 'mse':
+            criterion = nn.MSELoss()
+
+        for _ in range(epochs):
+            optimizer.zero_grad()
+
+            pred = self.decoder(rand_input)
+
+            loss = criterion(pred, output)
+            loss.backward()
+
+            optimizer.step()
+
+        return input
---
+++ SoundDSConv.py
@@ -0,0 +1,77 @@
+import torch
+import torchaudio
+from torchaudio import transforms
+import random
+from torch.utils.data import Dataset
+
+# ----------------------------
+# Sound Dataset
+# ----------------------------
+class SoundDSConv(Dataset):
+    def __init__(self, df, sr = 8000, duration = 10):
+        self.df = df
+        self.duration = duration*1000 # 10 seconds
+        self.sr = sr
+        self.channel = 1
+        self.spec_size = None
+
+    # ----------------------------
+    # Number of items in dataset
+    # ----------------------------
+    def __len__(self):
+        return len(self.df)
+    
+    # ----------------------------
+    # Choose sample duration
+    # ----------------------------
+    def set_sample_duration(self, duration):
+        self.duration = duration
+    
+    # ----------------------------
+    # Choose sample rate
+    # ----------------------------
+    def set_sample_rate(self, sample_rate):
+        self.sr = sample_rate
+    
+    # ----------------------------
+    # Get i'th item in dataset
+    # ----------------------------
+    def __getitem__(self, idx):
+        aud = torchaudio.load(self.df.loc[idx]['path_to_data'])
+        waveform, SAMPLE_RATE = aud
+
+        max_ms = self.duration
+        max_len = SAMPLE_RATE//1000 * max_ms
+        num_rows, sig_len = waveform.shape
+
+        # padding or truncating 
+        if (sig_len > max_len):
+          # Truncate the signal to the given length
+          waveform = waveform[:,:max_len]
+
+        elif (sig_len < max_len):
+            # Length of padding to add at the beginning and end of the signal
+            pad_begin_len = random.randint(0, max_len - sig_len)
+            pad_end_len = max_len - sig_len - pad_begin_len
+
+            # Pad with 0s
+            pad_begin = torch.zeros((num_rows, pad_begin_len))
+            pad_end = torch.zeros((num_rows, pad_end_len))
+
+            waveform = torch.cat((pad_begin, waveform, pad_end), 1)
+
+        waveform = waveform[0] # mix down
+
+        # Reduce the Sample Rate
+        resampler = transforms.Resample(orig_freq=SAMPLE_RATE, new_freq=self.sr)
+        waveform = resampler(waveform)
+
+        spectrogram = transforms.Spectrogram(n_fft=1024,
+                                             hop_length=512)
+
+        spec = spectrogram(waveform)
+
+        if self.spec_size == None:
+            self.spec_size = spec.shape
+
+        return spec.reshape(1, self.spec_size[0], self.spec_size[1])
